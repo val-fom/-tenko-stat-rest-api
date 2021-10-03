ed8b40c3ccf24566508f041359272de9f26ad338
Valentyn Fomenko <fomenko.vn@gmail.com>
2021-10-03 19:09:49 +0300

    install deps and get working standalone handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { HttpStatus } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const appService = appContext.get(AppService);
+
+  return {
+    body: appService.getHello(),
+    statusCode: HttpStatus.OK,
+  };
+};
